49063a78bf4443f6dcef3fcc99f96a8b4873629f
wenchijchang <heyyhey@hotmail.com>
2023-02-23 23:44:43 -0500

    updated settings.py and local settings

--- music_library_backend_project/settings.py
+++ music_library_backend_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e*yh%y_9w2ab!t=z+j*+6$e9ucc1pi8optavcr%may8p(lux6g'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,15 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'music_library_backend_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -125,3 +116,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL=True
+try:
+    from music_library_backend_project.local_settings import *
+except ImportError:
+    pass
