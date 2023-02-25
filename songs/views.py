95a7cebebd3ab6ebb63c678c24b58694a56725d3
wenchijchang <heyyhey@hotmail.com>
2023-02-25 12:26:07 -0500

    created POST and added status codes

--- songs/views.py
+++ songs/views.py
@@ -1,12 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Song
 from . serializers import SongSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
-    songs = Song.objects.all()
-
-    serializer = SongSerializer(songs, many=True)
-
-    return Response(serializer.data)
+    
+    if request.method == "GET":
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
