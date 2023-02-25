e3beae20c2e6f046414007f9b6200a44baf4a7b8
wenchijchang <heyyhey@hotmail.com>
2023-02-24 22:56:59 -0500

    updated project urls + updated GET request

--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from . serializers import SongSerializer
 
 @api_view(['GET'])
 def songs_list(request):
+    songs = Song.objects.all()
 
+    serializer = SongSerializer(songs, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
