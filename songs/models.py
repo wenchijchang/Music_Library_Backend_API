49302a90d993db40ca1c7092f68e82ab917fc319
wenchijchang <heyyhey@hotmail.com>
2023-02-26 00:14:44 -0500

    added likes column to song model file + updated SongSerializer Meta class fields + migrated again + created PUT for Add_Likes_to_Song + exported updated postman collection

--- songs/models.py
+++ songs/models.py
@@ -8,3 +8,4 @@ class Song(models.Model):
     album = models.CharField(max_length=255)
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
